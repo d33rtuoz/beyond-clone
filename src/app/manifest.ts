cbd29a8ae32a2254a2198a5385bd3cda52b5061a
d33rtuoz <d33rtuoz@gmail.com>
2025-06-24 08:59:49 +0300

    Manifest and icons.

---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Dungeon & Dragons Digital Game Companion',
+    short_name: 'D&Dvtt',
+    description:
+      'Create and manage your D&D characters and NPCs right on your device.',
+    start_url: '/',
+    display: 'standalone',
+    // TODO: Get theme from variable.
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  };
+}
