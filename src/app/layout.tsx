39ffedf98c2779e808d51ff8d41aad3dda6cb253
d33rtuoz <d33rtuoz@gmail.com>
2025-07-09 20:54:50 +0300

    Added default PWA files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,24 @@
 import { ThemeProvider } from '@/components/theme-provider';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 export const metadata: Metadata = {
   title: 'Dungeon & Dragons Digital Game Companion',
   description:
     'Create and manage your D&D characters and NPCs right on your device.',
+  manifest: '/manifest.json',
+  icons: {
+    icon: '/icon-192x192.png',
+    apple: '/icon-512x512.png',
+  },
 };
 
 const geist = Inter({
--- src/middleware.ts
+++ src/middleware.ts
@@ -23,5 +23,15 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|auth|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - manifest.json (web app manifest)
+     */
+    '/((?!api|auth|_next/static|_next/image|favicon.ico|manifest.json).*)',
+  ],
 };
