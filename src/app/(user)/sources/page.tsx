1d3c4c548fec5618fb63c2523e4c6cc7c82d7b05
d33rtuoz <d33rtuoz@gmail.com>
2025-07-12 00:06:39 +0300

    SourcesPage: search

--- src/app/(user)/sources/page.tsx
+++ src/app/(user)/sources/page.tsx
@@ -1,24 +1,50 @@
 import { Input } from '@/components/ui/input';
-import { H1, P } from '@/components/ui/typography';
+import { H1, H2, P } from '@/components/ui/typography';
 import { PAGES } from '@/constants/pages';
+import { getSources } from '@/server/sources.actions';
 import Form from 'next/form';
-import SourcesList from './sourcesList';
 
-export default function SourcesPage() {
+export default async function SourcesPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const sources = await getSources((await searchParams).search);
+
   return (
     <div className='flex flex-col gap-4'>
       <H1>Sources</H1>
       <P>Official books, rules for players and DM's. Adventures. Homebrews.</P>
-      <Form
-        action={PAGES.SOURCES}
-        className='flex flex-col gap-4'
-      >
-        <Input
-          type='text'
-          name='search'
-        />
-        <SourcesList />
+      <Form action={PAGES.SOURCES}>
+        <div className='flex w-full max-w-sm items-center gap-2'>
+          <Input
+            type='text'
+            placeholder='Search...'
+            name='search'
+            className='flex-1'
+          />
+          {/* <Button
+            variant='ghost'
+            size='icon'
+          >
+            <SlidersHorizontal />
+          </Button> */}
+        </div>
       </Form>
+      <>
+        <ul className='flex flex-col gap-4'>
+          {sources &&
+            sources.map((item) => (
+              <li
+                key={item.id}
+                className='p-4 bg-primary text-primary-foreground rounded-md'
+              >
+                <H2>{item.name}</H2>
+                <P>by {item.author}</P>
+              </li>
+            ))}
+        </ul>
+      </>
     </div>
   );
 }
--- src/server/sources.actions.ts
+++ src/server/sources.actions.ts
@@ -1,7 +1,23 @@
 'use server';
 
 import { db } from '@/db';
+import { sources } from '@/db/schema';
+import { ilike, or, sql } from 'drizzle-orm';
 
-export async function getSources() {
-  return await db.query.sources.findMany();
+export async function getSources(query: string | string[] | undefined) {
+  switch (typeof query) {
+    case 'undefined':
+      return await db.query.sources.findMany();
+    case 'string':
+      return await db
+        .select()
+        .from(sources)
+        .where(
+          or(
+            query ? ilike(sources.name, `%${query}%`) : undefined,
+            query ? ilike(sources.abbreviatedName, `%${query}%`) : undefined,
+            query ? ilike(sources.author, `%${query}%`) : undefined
+          )
+        );
+  }
 }
